fe3fde1144bf36ed23e21aafb6365a2f80093565
10tanmay100 <chakrabortytanmay744@gmail.com>
2024-01-24 02:36:14 +0530

    cicd added unit test will be added later

--- init-setup.sh
+++ init-setup.sh
@@ -1,10 +1,10 @@
 echo [$(date)]: "START"
 echo [$(date)]: "creating env with python 3.8 version"
-conda create --prefix ./env python=3.8 -y
+conda create --prefix ./env python=3.11 -y
 echo [$(date)]: "activating the environment"
 source activate ./env
 echo [$(date)]: "installing the dev requirements"
-pip install -r requirements_dev.txt
+pip install -r requirements.txt
 echo [$(date)]: "END"
 
 
