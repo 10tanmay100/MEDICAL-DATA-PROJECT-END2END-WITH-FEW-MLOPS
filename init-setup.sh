0e4baff3fbacd180bda547f0cbf646ef29ee02cd
10tanmay100 <chakrabortytanmay744@gmail.com>
2024-01-24 11:48:08 +0530

    cicd added but unit test cases will be added later

--- init-setup.sh
+++ init-setup.sh
@@ -1,5 +1,5 @@
 echo [$(date)]: "START"
-echo [$(date)]: "creating env with python 3.8 version"
+echo [$(date)]: "creating env with python 3.11 version"
 conda create --prefix ./env python=3.11 -y
 echo [$(date)]: "activating the environment"
 source activate ./env
